648764a11cd43b0750b879e9f682b034c454e72f
Andre-klarmann <112667579+Andre-klarmann@users.noreply.github.com>
2024-01-26 18:22:29 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,6 @@ import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/timeline";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://kjrzkvvspoypfgxewzrg.supabase.co";
-const supabaseKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqcnprdnZzcG95cGZneGV3enJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgzOTMxNjUsImV4cCI6MTk4Mzk2OTE2NX0.8DNk7T35selbco-AFVNVjdWKuftbKsJAHezjdUoAe_o";
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function Home() {
 	const [valorDoFiltro, setValorDoFiltro] = React.useState("");
